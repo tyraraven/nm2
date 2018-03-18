6451b65626b48a63c5b1dc0df6dc711cfaf97e33
nivin <mtt_hll@hotmail.com>
2018-03-18 00:56:12 -0400

    Basic intro in, starting to work on tutorial/very early game systems

--- js/actions.js
+++ js/actions.js
@@ -1,13 +1,38 @@
+// Awareness Intro Game Stat
+var isAware = false;
+var awareness = 0;
+var tutorial = 0;
+var training = 0;
 
-var curiosity = 0;
-
-function testAlert() {
-    if (curiosity < 5) {
-        updateMainStory(phaseOneMessages[curiosity]);
-        curiosity++;
-    } else if (curiosity >= 3) {
-
+function lookAction() {
+    if (!isAware) {
+        if (awareness < phaseOneMessages.length) {
+            updateMainStory(phaseOneMessages[awareness]);
+            awareness++;
+        }
+        if (awareness == 10) {
+            document.getElementById('look').innerHTML = 'Look Around';
+        }
+        if (awareness == phaseOneMessages.length) {
+            isAware = true;
+        }
+    } else {
+        updateMainStory(tutorialMessages[tutorial]);
+        if (tutorial == 4) {
+            document.getElementById('mainStats').style.display='block';
+            document.getElementById('train').style.display='block';
+        }
+        tutorial++;
     }
+}
+
+function trainAction() {
+    incSenseOfSelf();
+}
+
+function incSenseOfSelf() {
+    training++;
+    document.getElementById('sosValue').innerHTML = training;
 }
 
 function updateMainStory(message) {
--- js/messages.js
+++ js/messages.js
@@ -1,7 +1,40 @@
+// Initial Intro Messages
 var phaseOneMessages = [];
 phaseOneMessages.push('The darkness is all around you, no matter how hard you try you can\'t seem to make out anything.');
 phaseOneMessages.push('Its blackness is everywhere, but somehow you know that YOU are still there, how long have you been like this?');
 phaseOneMessages.push('The more you consider it, this blackness can\'t be all that there is, after all you are there, and you are obviously not nothing.');
 phaseOneMessages.push('Could this just be you waking up from a deep sleep, if so why can you not see anything?');
 phaseOneMessages.push('You struggle to wake up.');
-phaseOneMessages.push('You struggle to wake up.');
+phaseOneMessages.push('You continue to struggle to wake up.');
+phaseOneMessages.push('Slowly the edges of the darkness start to brighten, something is happening.');
+phaseOneMessages.push('Light is starting to fill your vision.');
+phaseOneMessages.push('A horizontal slit goes across the center of your vision, a bright slash against the darkness.');
+phaseOneMessages.push('You can see a room, flat and featureless, you are lying on the floor.');
+//Awake
+phaseOneMessages.push('The room is stark white and nearly featureless.  There is some kind of hissing and crackling sound that is fading away.');
+phaseOneMessages.push('Looking down you can see your body, but it is strange to behold. ' +
+    'You have a white nearly featureless form with segmented body parts.  Your limbs are blocky and smooth, but at least you have to proper number of them.');
+phaseOneMessages.push('You do not think this is how you are supposed to look, you are.. human?  At least you think so.');
+phaseOneMessages.push('In the center of your chest there is a strange blue disk, it seems like this is where the hissing sound was coming from as you can still faintly hear it coming from the device.');
+phaseOneMessages.push('The device has strange markings along the outside of it, which you know to be writing.  You however cannot seem to read currently, which is distressing to say the very least.');
+phaseOneMessages.push('Turning your attention away from the mysterious device you scan the rest of the room, now that you are more awake you can make out geometric lines and patterns over the walls.' +
+'  There are various rectangles and other shapes adorning nearly everything.  Perhaps this room isn\'t as featureless as it originally appeared.');
+phaseOneMessages.push('Intrigued, you attempt to get up from the floor so you can take a better look at the walls.')
+phaseOneMessages.push('Getting up is.... difficult.  Whatever body this is, it clearly isn\'t yours.  But after a bit of trial and error you manage to get yourself up.');
+phaseOneMessages.push('Walking over to the wall, you make out a large rectangle near one end of the room, could that be... a door?');
+phaseOneMessages.push('Yes, the rectangle seems to be in the shape of a door, though you can see no obvious way to open it.  Suddenly the "door" shudders violently! A massive sound comes from the panel.');
+phaseOneMessages.push('Alarmed you try to step back, but you are still uncoordinated with this body and you stumble back to the ground.');
+phaseOneMessages.push('The door crashes open and before you stands a stark white creature, remarkably similar to yourself.  It has glowing red eyes but no device central on its chest.');
+phaseOneMessages.push('The creature looks like it has been in some kind of awful battle, gashes all over its form.  Wires poke out from some of the wounds, sparking off electricity as it moves');
+phaseOneMessages.push('Suddenly the creature speaks "Cannot stop, I\'m so sorry.".  At which point the creature lurches towards you. ');
+phaseOneMessages.push('It tears into your form, smashing your limbs and tearing you apart, as the blackness starts to take you again you suddenly see a massive flash of blue light emanating from your chest.');
+phaseOneMessages.push('The blackness fills you up, but as it does new words start to appear before you.  "Emergency Safety Triggered, Engaging in Temporal Transference!"');
+phaseOneMessages.push('Suddenly the world is streaming backwards, and you stand back from your "body" a bit watching it all happen.  Quickly you see yourself walk backwards from the wall, awkwardly fall back to the gounrd and close your eyes.');
+phaseOneMessages.push('At that point you feel yourself being drawn back into the body, but before that happens several messages appear before you');
+
+var tutorialMessages = [];
+tutorialMessages.push('Welcome to Necromental!  This is an game about discovery and overcoming obstacles, as well as other so far undisclosed things.');
+tutorialMessages.push('Your first goal is right before you, somehow you need to not get killed by the being that is about to smash through that door.  How you do it is up to you, but understand that you will not be successful on your first try.  As you die, you will be given the chance to improve yourself based on the actions that you took.');
+tutorialMessages.push('Depending on your current statistics more options will open up to you on the interface.  Be warned however that you will not keep all of the progress on a given run, instead having some of that progress converted into permanent ability modifiers/multipliers.');
+tutorialMessages.push('Above you you can now see a window has appeared, it has a listing of your current statistics, as well as a brief explanation about what that statistic currently does for you.');
+
